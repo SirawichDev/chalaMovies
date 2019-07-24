461a82525a9dab12dc1dfb3e52b2a0b8c5612e9b
SirawichDev <sirawit0676@gmail.com>
2019-07-24 18:29:36 +0700

    _apikey variable

--- lib/resources/api_provider.dart
+++ lib/resources/api_provider.dart
@@ -1,16 +1,19 @@
-import 'package:http/http.dart';
-import '../model/movie_model.dart';
 import 'dart:convert';
+import 'package:http/http.dart' show Client;
+import '../model/movie_model.dart';
 
-class ChalaMovieProvider {
+
+class ChalaMovieProvider  {
   Client cli = Client();
-  final apiKey = "";
-  final baseUrl = "";
+  final _apiKey = "45802feae9d98fc53bf0b8c6519d6230";
+  final _baseUrl = "http://api.themoviedb.org/3/movie";
 
-  Future<movieModel> fetchList() async {
-    final res = await cli.get("");
+  Future<MovieModel> fetchList() async {
+  print("joined");
+    final res = await cli.get("https://api.themoviedb.org/3/movie/now_playing?api_key=$_apiKey");
+    print(res.body.toString());
     if (res.statusCode == 200) {
-      return movieModel.fromJson(json.decode(res.body));
+      return MovieModel.fromJson(json.decode(res.body));
     } else {
       throw Exception('failed to load list');
     }
