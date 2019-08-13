6f924ae52786d7932af3b0f615f3a42a8eb62942
SirawichDev <sirawit0676@gmail.com>
2019-08-14 02:24:47 +0700

    GenresList provider

--- lib/resources/api_provider.dart
+++ lib/resources/api_provider.dart
@@ -1,19 +1,42 @@
 import 'dart:convert';
+import 'package:chalaMovie/model/genre_model.dart';
 import 'package:http/http.dart' show Client;
 import '../model/movie_model.dart';
 
 class ChalaMovieProvider {
   Client cli = Client();
   final _apiKey = "45802feae9d98fc53bf0b8c6519d6230";
-  final _baseUrl = "http://api.themoviedb.org/3/movie";
+  final _baseMovieUrl = "http://api.themoviedb.org/3/movie";
+  final _baseGenreUrl = "http://api.themoviedb.org/3/genre";
 
-  Future<MovieModel> fetchList() async {
-    print("joined");
-    final res = await cli
-        .get("https://api.themoviedb.org/3/movie/now_playing?api_key=$_apiKey");
+  Future<MovieModel> fetchCurrentList(bool isRecent) async {
+    print("now playing joined");
+    final res = await cli.get(_baseMovieUrl + "/now_playing?api_key=$_apiKey");
     print(res.body.toString());
     if (res.statusCode == 200) {
-      return MovieModel.fromJson(json.decode(res.body));
+      return MovieModel.fromJson(json.decode(res.body), isRecent);
+    } else {
+      throw Exception('failed to load list');
+    }
+  }
+
+  Future<MovieModel> fetchMostWatcherList(bool isRecent) async {
+    print("popular joined");
+    final res = await cli.get(_baseMovieUrl + "/popular?api_key=$_apiKey");
+    print(res.body.toString());
+    if (res.statusCode == 200) {
+      return MovieModel.fromJson(json.decode(res.body), isRecent);
+    } else {
+      throw Exception('failed to load list');
+    }
+  }
+
+  Future<GenreModel> fetchGenresList() async {
+    print("genres joined");
+    final res = await cli.get(_baseGenreUrl + "/movie/list?api_key=$_apiKey");
+    print(res.body.toString());
+    if (res.statusCode == 200) {
+      return GenreModel.fromJson(json.decode(res.body));
     } else {
       throw Exception('failed to load list');
     }
