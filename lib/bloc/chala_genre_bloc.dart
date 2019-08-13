77962db7459061bd1ef33e46dd826266d63306ff
SirawichDev <sirawit0676@gmail.com>
2019-08-14 02:20:59 +0700

    genre bloc created

---
+++ lib/bloc/chala_genre_bloc.dart
@@ -0,0 +1,21 @@
+import 'package:chalaMovie/model/genre_model.dart';
+import 'package:chalaMovie/resources/repo.dart';
+import 'package:rxdart/rxdart.dart';
+
+class ChalaGenreBloc {
+  final repo = Repo();
+  final genreFetch = PublishSubject<GenreModel>();
+
+  Observable<GenreModel> get allGenres => genreFetch.stream;
+
+  fetchAllGenre() async {
+    GenreModel mModel = await repo.fetchAllGenreList();
+    genreFetch.sink.add(mModel);
+  }
+
+  dispose() {
+    genreFetch.close();
+  }
+}
+
+final bloc = ChalaGenreBloc();
