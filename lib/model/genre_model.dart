aa42b759578addd00611477891437418d55e119f
SirawichDev <sirawit0676@gmail.com>
2019-08-16 13:20:45 +0700

    .

--- lib/model/genre_model.dart
+++ lib/model/genre_model.dart
@@ -7,8 +7,20 @@ class GenreModel {
       Result result = Result(parsedJson['genres'][i]);
       temp.add(result);
     }
-
+    temp = temp.toSet().toList();
     results = temp;
+  }
+
+  List<Result> get get_genres => results;
+
+  String get_genre(List<int> ids) {
+    ids = ids.toSet().toList();
+    String mygenre = "";
+    for (var i = 0; i < ids.length; i++) {
+      mygenre += results.where((user) => user.id == ids[i]).first.name + ",";
+    }
+    mygenre = mygenre.substring(0, mygenre.length -1);
+    return mygenre;
   }
 }
 
